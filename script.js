92f0e0f22e101d1b9283fce1b5b41ff0db9001d8
Eric Wu <ericwu694@gmail.com>
2024-03-23 17:53:04 -0400

    Completed portfolio, holder images still need to be changed

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-const navElement = document.querySelector(".nav");//targetting hamburger menu that will add or remove the open class within it
-const hamburgerElement = document.querySelector(".hamburger");
-
-hamburgerElement.addEventListener('click', () => {
-    navElement.classList.toggle("nav--open");
-    hamburgerElement.classList.toggle("hamburger--open");
-}); 
-
-navElement.addEventListener("click", () => {
-    navElement.classList.remove("nav--open");
-    hamburgerElement.classList.remove("hamburger--open");
-});
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
